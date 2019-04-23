c670aa0afd652d07e86e89795b2920766e329478
hooN <johnhpark11@gmail.com>
2019-04-23 23:43:05 +0900

    Set Layout : Weather rendering component, create-react-app

---
+++ src/component/DayWeather.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DayWeather = (props) => {
+//property로 부터 날짜 및 날씨 정보를 받음
+    return(
+        render(
+
+        )
+    )
+//날씨 카드 한 장 rendering 하기
+}
+
+export default DayWeather;
