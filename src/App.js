200ae2041fd456059cab8a65067028e0c2f7d75d
hooN <johnhpark11@gmail.com>
2019-05-12 20:47:02 +0900

    Rendering Graph : Coverage Problem Solved

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ const todayDate = new Date();
 class App extends React.Component {
 
   state = {
-    lowTemp : 12,
-    highTemp : 18,
-    day : todayDate.getDay(),
-    weather : 0,
-    loc : {
-      city : 'Busan',
-      gu : 'Buk',
-      dong : 'Deokcheon'
+    lowTemp: 12,
+    highTemp: 18,
+    day: todayDate.getDay(),
+    weather: 0,
+    loc: {
+      city: 'Busan',
+      gu: 'Buk',
+      dong: 'Deokcheon'
     }
   }
 
@@ -28,26 +28,26 @@ class App extends React.Component {
     rain : 3  storm : 4
   */
 
-  render() {
-    return (
-      <div className="Weather">
-        <RenderInf day={this.state.day} loc={this.state.loc}/>
-        <Graph />
-        <div className="d-flex flex-row mb-3">
-          <DayWeather date={this.state.day} weather={this.state.weather}/>
-          <DayWeather date={(this.state.day+1)%7} weather={this.state.weather+1} lowTemp={this.state.lowTemp} highTemp={this.state.highTemp}/>
-          <DayWeather date={(this.state.day+2)%7} weather={this.state.weather+2} />
-          <DayWeather date={(this.state.day+3)%7} weather={this.state.weather+3} />
-          <DayWeather date={(this.state.day+4)%7} weather={this.state.weather+4} />
-          <DayWeather date={(this.state.day+5)%7} weather={this.state.weather} />
-          <DayWeather date={(this.state.day+6)%7} weather={this.state.weather+1} />
-          <DayWeather date={(this.state.day+7)%7} weather={this.state.weather+2} />
-        </div>
-
+render() {
+  return (
+    <div className="Weather">
+      <RenderInf day={this.state.day} loc={this.state.loc} />
+      <Graph />
+      <div className="d-flex flex-row mb-3">
+        <DayWeather date={this.state.day} weather={this.state.weather} />
+        <DayWeather date={(this.state.day + 1) % 7} weather={this.state.weather + 1} lowTemp={this.state.lowTemp} highTemp={this.state.highTemp} />
+        <DayWeather date={(this.state.day + 2) % 7} weather={this.state.weather + 2} />
+        <DayWeather date={(this.state.day + 3) % 7} weather={this.state.weather + 3} />
+        <DayWeather date={(this.state.day + 4) % 7} weather={this.state.weather + 4} />
+        <DayWeather date={(this.state.day + 5) % 7} weather={this.state.weather} />
+        <DayWeather date={(this.state.day + 6) % 7} weather={this.state.weather + 1} />
+        <DayWeather date={(this.state.day + 7) % 7} weather={this.state.weather + 2} />
       </div>
 
-    );
-  }
+    </div>
+
+  );
+}
 }
 
 export default App;
--- src/component/Graph.js
+++ src/component/Graph.js
@@ -1,21 +1,78 @@
 import React from 'react';
 import Billboard from 'billboard.js';
+import BillboardChart from "react-billboardjs";
+import "react-billboardjs/lib/billboard.css";
 
-const Graph = (props) => {
+const CHART_DATA = {
+    columns : [
+        ["temp", 24, 23, 19, 17, 15, 13, 11, 14, 21]
+    ],
+    type : "line"
+};
 
+export default class LineChart extends React.Component{
+    render(){
+        return(
+            <BillboardChart data={CHART_DATA}></BillboardChart>
+        )
+    }
+}
+/*
     let chart = Billboard.generate({
-        bindto : "#chart",
+        bindto : "#bind",
+
+        size : {
+            height : 250,
+            width : 800
+        },
+
         data: {
             type : "line",
             columns : [
-                ["data1", 24, 23, 19, 17, 15, 14, 15, 21, 24]
+                ["temperature", 24, 23, 19, 17, 15, 14, 15, 21, 24]
             ]
+        },
+        
+        axis : {
+            x : {
+                type : "category",
+                categories : [
+                    "PM 3시",
+                    "PM 6시",
+                    "PM 9시",
+                    "AM 0시",
+                    "AM 3시",
+                    "AM 6시",
+                    "AM 9시",
+                    "PM 12시",
+                    "PM 3시"
+                ]
+            }
+        },
+        area : {
+            above : false
+        },
+
+        point : {
+            show : false
         }
     });
 
+    chart.axis.range({
+        min : {y : 10},
+        max : {y : 30},
+    });
+
+    chart.data.colors({
+        temperature : "#FF0000"
+    });
+
     return(
-        <div id="chart"></div>
+        <div id="bind">
+            helloS
+        </div>
     )
 }
 
 export default Graph;
+*/
